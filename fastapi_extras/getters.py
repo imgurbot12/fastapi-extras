c27eede27eb8fe27281bb15dfce493a52f8bbc3e
imgurbot12 <imgurbot12@gmail.com>
2023-10-09 13:15:00 -0700

    feat: getter updates

--- fastapi_extras/getters.py
+++ fastapi_extras/getters.py
@@ -3,13 +3,14 @@ FastAPI Simple Synchronous Getters
 """
 import inspect
 import functools
-from typing import Any, Callable, Type, TypeVar
+from typing import Any, Callable, Generator, Type, TypeVar
 
 from fastapi import Depends, Form, HTTPException, Query
 from fastapi.requests import Request
 from fastapi.datastructures import FormData
 from fastapi.exceptions import RequestValidationError
 
+from pyderive.extensions.serde import to_dict
 from pyderive.dataclasses import MISSING, FIELD_ATTR
 from pyderive.extensions.validate import BaseModel, FieldValidationError
 
@@ -88,14 +89,19 @@ def as_form(model: Type[Model]) -> Model:
     """
     return _model_depends(Form, model)
 
-def as_session(model: Type[BaseModel], status: int = 401):
+def as_session(model: Type[Model], status: int = 401):
     """
     generate dynamic depends function to parse session-data as pydantic model
     """
-    async def func(req: Request) -> model:
+    def func(req: Request) -> Generator[Model, None, None]:
+        # supply session model to function
         try:
             session = req.scope.get('session', {})
-            return model(**session)
+            value   = model(**session)
+            yield value
         except FieldValidationError:
             raise HTTPException(status, 'invalid session state')
+        # re-validate and pass model back to storage after completion
+        value.validate()
+        req.scope['session'] = to_dict(value)
     return Depends(func)
